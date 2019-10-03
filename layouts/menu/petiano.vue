b3e6a0f003adb65c59519b277d16545491f0e0a1
Italo Epifanio <italoepifaniols@gmail.com>
2019-10-03 20:15:44 -0300

    Inicia cadastro de evento

--- layouts/menu/petiano.vue
+++ layouts/menu/petiano.vue
@@ -6,48 +6,7 @@
       <main class="main">
         <breadcrumb :list="list"/>
         <div class="container-fluid">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <a class="btn btn-sm btn-primary float-right" style="color: white">Adicionar evento</a>
-                        <div class="table-responsive" v-if="eventos.length > 0">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                        <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="row" v-else>
-                            Nenhum evento cadastrado
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <nuxt />
         </div>
       </main>
     </div>
@@ -57,9 +16,8 @@
 
 <script>
 import nav from './petiano'
-import auth from '../../auth'
 import { Header as AppHeader, Sidebar, Footer as AppFooter, Breadcrumb } from '~/components/'
-import axios from "axios"
+
 export default {
   name: 'full',
   components: {
@@ -70,8 +28,7 @@ export default {
   },
   data () {
     return {
-      nav: nav.items,
-      eventos: []
+      nav: nav.items
     }
   },
   computed: {
@@ -79,12 +36,5 @@ export default {
       return this.$route.matched
     }
   },
-  mounted(){
-      console.log(auth);
-    let vm = this;
-    axios.get('http://epet.imd.ufrn.br/service/api/eventos', auth).then(
-        (data) => { vm.eventos = data }
-    );
-  }
 }
 </script>
---
+++ pages/eventos/create.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <form @submit="submitForm">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Titulo</label>
+            <input type="text" class="form-control" v-model="form.titulo" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Descrição</label>
+            <input type="text" class="form-control" v-model="form.descricao" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Local</label>
+            <input type="text" class="form-control" v-model="form.local" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Carga Horaria</label>
+            <input type="number" class="form-control" v-model="form.qtdCargaHoraria" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Dias</label>
+            <input type="number" class="form-control" v-model="form.qtdDias" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Vagas</label>
+            <input type="number" class="form-control" v-model="form.qtdVagas" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Valor</label>
+            <input type="number" min="0" pattern="\d+" class="form-control" v-model="form.valor" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Data Inscrição</label>
+            <input type="date" class="form-control" v-model="form.d_inscricao" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Data Final das Inscrição</label>
+            <input type="date" class="form-control" v-model="form.d_inscricao_fim" />
+          </div>
+          <div class="form-group">
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import auth from "../../auth";
+
+export default {
+  layout: "menu/petiano",
+  data() {
+    return {
+      form: {
+        titulo: "teste",
+        descricao: "testando",
+        local: "rua dos testes",
+        qtdCargaHoraria: 0,
+        qtdDias: 0,
+        qtdVagas: 0,
+        valor: 0,
+        d_inscricao_fim: "",
+        d_inscricao: "",
+        ativo: true,
+      }
+    };
+  },
+  methods: {
+    submitForm(e) {
+      axios
+        .post("http://epet.imd.ufrn.br/api/eventos-cadastrar", auth, this.form)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(this.form));
+
+      e.preventDefault();
+    }
+  }
+};
+</script>
