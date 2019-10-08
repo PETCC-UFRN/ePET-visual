e85f04e49337435ce2b15e52736de50dbfbcf7e5
confuzo <abraaovld@gmail.com>
2019-10-07 21:29:14 -0300

    organizadores page implemented

---
+++ pages/organizadores/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <a
+      class="btn btn-sm btn-primary float-right"
+      style="color: white"
+      href="organizadores/create"
+    >Adicionar Organizador</a>
+    <div v-if="eventos.length > 0">
+      <b-card header="Organizadores cadastrados">
+        <!-- TODO::remover esse style -->
+        <!--<a
+          class="btn btn-sm btn-primary float-right"
+          style="color: white"
+          href="participantes/create"
+        >Adicionar evento</a>-->
+        <b-table
+          responsive="sm"
+          :items="eventos"
+          :current-page="currentPage"
+          :bordered="true"
+          :per-page="10"
+          :fields="fields"
+        >
+          <!--<template v-slot:cell(ativo)="row">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" :checked="row.item.ativo" disabled>
+              </div>
+          </template>-->
+          <template v-slot:cell(actions)="row">
+            <!--<b-button @click="confirmar(row.item.idParticipantes)" class="btn btn-sm btn-success" v-show="! row.item.ativo">Confirmar</b-button>-->
+            <b-button @click="del(row.item.idOrganizadores, row.index)" class="btn btn-sm btn-danger">Deletar</b-button>
+          </template>
+        </b-table>
+        <nav>
+          <b-pagination
+            :total-rows="eventos.length"
+            :per-page="10"
+            v-model="currentPage"
+            prev-text="Prev"
+            next-text="Next"
+            hide-goto-end-buttons
+          />
+        </nav>
+      </b-card>
+    </div>
+    <div class="row" v-else>Nenhum Organizador cadastrado</div>
+  </div>
+</template>
+
+<script>
+import axios from "../../axios";
+
+export default {
+  name: "dashboard",
+  /* TODO:: Esse layout será apresentado tanto pro petiano quando pro coordenador
+  depois será necessário uma lógica pra chamar o layout dependendo do tipo de usuário
+  logado. No momento trabalharei apenas com os petianos. */
+  layout: "menu/petiano",
+  data() {
+    return {
+      eventos: [],
+      currentPage: 1,
+      fields: [
+        { key: "pessoa.nome", label: "Nome do usuário", sortable: true },
+        { key: "evento.titulo", label: "Nome do evento", sortable: true },
+        /*{ key: "confirmado", sortable: true },
+        { key: "espera", sortable: true },
+        //{ key: "ativo", sortable: true },*/
+        { key: "actions", sortable: true },
+      ]
+    };
+  },
+  mounted() {
+    axios.get("organizadores").then(res => {
+      this.eventos = res.data.content;
+    });
+  },
+  methods: {
+    del(id, rowId){
+      //console.log(id);
+      axios.delete("organizadores-remove/" + id).then(() => {
+        this.eventos.splice(rowId, 1);
+        alert('Participante removido com sucesso');
+      });
+    },
+    /*confirmar(id){
+      axios.post("participantes-confirmar/" + id).then(() => {
+        // para não ter que atualizar os eventos em tempo real forçarei a página a atualizar
+        alert('Participante ativado com sucesso');
+        let vm = this;
+        setTimeout(function(){
+          location.reload()
+        }, 1500)
+      });
+    }*/
+  }
+};
+</script>
