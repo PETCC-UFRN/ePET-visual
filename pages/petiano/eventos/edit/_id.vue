ff2a644838904e8c03bfd56a0dafdf69feab79c4
Ítalo Epifãnio <italoepifaniols@gmail.com>
2020-01-31 09:52:23 -0300

    Cria visualização de noticia para usuário. Inicia edição de notícia por parte do petiano

--- pages/petiano/eventos/edit/_id.vue
+++ pages/petiano/eventos/edit/_id.vue
@@ -11,41 +11,7 @@
             <label for="exampleFormControlInput1">Titulo</label>
             <input type="text" class="form-control" v-model="form.titulo" />
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Descrição</label>
-            <input type="text" class="form-control" v-model="form.descricao" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Local</label>
-            <input type="text" class="form-control" v-model="form.local" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Carga Horaria</label>
-            <input type="number" class="form-control" v-model="form.qtdCargaHoraria" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Dias</label>
-            <input type="number" class="form-control" v-model="form.qtdDias" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Vagas</label>
-            <input type="number" class="form-control" v-model="form.qtdVagas" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Valor</label>
-            <input type="number" min="0" pattern="\d+" class="form-control" v-model="form.valor" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Data Inscrição</label>
-            <input type="date" class="form-control" v-model="form.d_inscricao" />
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Data Final das Inscrição</label>
-            <input type="date" class="form-control" v-model="form.d_inscricao_fim" />
-          </div>
-          <div class="form-group">
-            <button type="submit">Enviar</button>
-          </div>
+          
         </form>
       </div>
     </div>
