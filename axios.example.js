06a4f26c4960bbf32d32f4aa44de3f3faa770e10
jhon3 <cabral.jhon00@gmail.com>
2020-03-30 20:23:05 -0300

    autenticacao ok

--- axios.example.js
+++ axios.example.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
+import Cookies from "js-cookie";
 
 export default axios.create({
-    baseURL: "http://epet.imd.ufrn.br/service/api",
-    auth: {
-        username: '',
-        password: ''
-    }
+    baseURL: "http://epet.imd.ufrn.br:8080/api",
+    headers: {'Authorization': `${Cookies.get("auth")}`},
 })
+
