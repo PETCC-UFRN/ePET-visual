6e81b4cf3ec1a88a63e776f8583b444bdfb7ff5f
Samuel Lucas <you@example.com>
2020-04-14 07:14:47 -0300

    Modificando os negocios no usuario

---
+++ pages/usuario/eventos/eventos-cadastrados/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-card
+      v-if="loaded"
+      header="Evento"
+      header-tag="header"
+      :footer="'Local: ' + evento.local"
+      :title="evento.titulo"
+    >
+      <b-card-text>
+        <span v-html="evento.descricao"></span>
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import axios from "~/axios";
+
+export default {
+  name: "dashboard",
+  layout: "menu/usuario",
+  data() {
+    return { evento: [], loaded: false };
+  },
+  mounted() {
+    axios.get("eventos/" + this.$route.params.id).then(res => {
+      this.evento = res.data;
+      this.loaded = true;
+    });
+  }
+};
+</script>
