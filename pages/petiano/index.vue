187e6e22eb4d776315081e4e8e5c166a13b54efb
Samuel Lucas <you@example.com>
2020-04-20 09:41:36 -0300

    Modificando home de dashboard tutor

--- pages/petiano/index.vue
+++ pages/petiano/index.vue
@@ -8,43 +8,71 @@
         <div></div>
       </div>
     </div>
-    <div class="row" v-else>
-      <div class="col-md-6">
-        <span class="lead"><h1>Bem vindo!</h1></span>
-      </div>
-      <div class="col-md-6">
-        <b-card >
-          <template v-slot:header>
-            <h4 class="mb-0">Notícias</h4>
-          </template>
-          <b-table
-            responsive="sm"
-            hover
-            :items="resNoticias"
-            :current-page="currentPage"
-            :bordered="true"
-            :per-page="10"
-            :fields="fields"
-          >
-            <template v-slot:cell(actions)="row">
-              <b-button
-                :href="'/petiano/noticia/visualize/' + row.item.idNoticia"
-                class="btn btn-sm btn-warning"
-              ><i class="fa fa-eye" aria-hidden="true"></i>
- Visualizar</b-button>
-            </template>
-          </b-table>
-          <nav>
-            <b-pagination
-              :total-rows="resNoticias.length"
-              :per-page="10"
-              v-model="currentPage"
-              prev-text="Anterior"
-              next-text="Próximo"
-              hide-goto-end-buttons
-            />
-          </nav>
-        </b-card>
+    <div v-else>
+      <div class="row">
+        <div class="col-md-6">
+          <b-card
+            header-tag="header"
+            footer-tag="footer">
+            <div slot="header">
+              <b-row>
+                <b-col>
+                  <h3><i class="fa fa-align-justify"></i>  Próximos eventos</h3>
+                </b-col>
+              </b-row>
+            </div>
+            <div role="tablist" v-for="evento in eventos" :key="evento.idEvento">
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-btn block href="#" v-b-toggle="'accordion' + evento.idEvento" variant="primary">{{evento.titulo}}</b-btn>
+                </b-card-header>
+                <b-collapse :id="'accordion' + evento.idEvento" accordion="my-accordion" role="tabpanel">
+                  <b-card-body>
+                    <p class="card-text">
+                      <strong>Descrição: </strong>{{evento.descricao}}
+                    </p>
+                    <div class="card-text">
+                      <p  class="mt-0 mb-0"><b>Período de inscrição:</b> {{evento.d_inscricao}} a {{evento.d_inscricao_fim}}.</p>
+                      <p class="mt-0 mb-0"><b>Número de vagas:</b> {{evento.qtdVagas}}.</p>
+                      <p class="mt-0 mb-0"><b>Local:</b> {{evento.local}}.</p>
+                      <p class="mt-0 mb-0"><b>Carga horária:</b> {{evento.qtdCargaHoraria}}h.</p>
+                      <p class="mt-0 mb-0"><b>Valor da inscrição:</b> R$ {{evento.valor}},00.</p>
+                    </div>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+            </div>
+          </b-card>
+        </div>
+        <div class="col-md-6">
+          <b-card
+            header-tag="header"
+            footer-tag="footer">
+            <div slot="header">
+              <b-row>
+                <b-col>
+                  <h3><i class="fa fa-newspaper-o" aria-hidden="true"></i> Notícias recentes</h3> 
+                </b-col>
+              </b-row>
+            </div>
+            <b-list-group>
+              <div v-for="noticia in noticias" :key="noticia.idNoticia">
+          
+                <b-list-group-item href="#" class="flex-column align-items-start mb-2">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{noticia.titulo}}</h5>
+                    <small class="text-muted">{{noticia.inicio_exibicao}}</small>
+                  </div>
+                  <p class="mb-1">
+                    {{noticia.corpo}}
+                  </p>
+                  <small class="text-muted"><em>Postado por {{noticia.petiano.pessoa.nome}}</em></small>
+                </b-list-group-item>
+                
+              </div>  
+            </b-list-group>
+          </b-card>
+        </div>
       </div>
     </div>
   </div>
@@ -60,7 +88,8 @@ export default {
   data: function() {
     return {
       isLoading: true,
-      noticias: {}, // requisicao de noticias
+      noticias: [], // requisicao de noticias
+      eventos: [],
       currentPage: 1,
       fields: [
         { key: "titulo", sortable: true, label: "Título" },
@@ -70,9 +99,13 @@ export default {
     };
   },
   mounted() {
-    axios.get("noticia").then(res => {
-      this.noticias = res.data.content;
+    axios.get("noticia/?page=0").then(res => {
+      this.noticias = res.data.content.slice(0,3);
       this.isLoading = false;
+    })
+
+    axios.get("eventos-abertos").then(res => {
+      this.eventos = res.data;
     });
   },
   computed: {
@@ -83,4 +116,5 @@ export default {
     }
   }
 };
+
 </script>
