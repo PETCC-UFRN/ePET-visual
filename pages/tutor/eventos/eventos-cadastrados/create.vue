160beebf935e6fd5e2c221673bda74b953a78873
Samuel Lucas <samuellucas97@ufrn.edu.br>
2020-05-12 05:28:20 -0300

    Modificando telas de index de eventos e criacao de eventos

--- pages/tutor/eventos/eventos-cadastrados/create.vue
+++ pages/tutor/eventos/eventos-cadastrados/create.vue
@@ -4,7 +4,7 @@
       <div class="card-header">
         <b-row>
           <b-col>
-            <h3><i class="fa fa-edit"></i> Cadastrar evento</h3>
+            <h2><i class="fa fa-edit"></i> Cadastrar evento</h2>
           </b-col>
           <b-col>
             <div class="card-actions">
@@ -16,68 +16,175 @@
       <div class="card-body">
         <form @submit.prevent="submitForm">
           <div class="form-group">
-            <label for="exampleFormControlInput1"><strong>Título:</strong></label>
-            <input type="text" class="form-control" placeholder="Digite o título" v-model="form.titulo" />
+            <label for="titulo"><strong>Título</strong></label>
+            <input id="titulo"
+              type="text" class="form-control" 
+              required placeholder="Digite o título"
+              v-model="form.titulo" />
           </div>
           <div class="form-group">
-            <label for="exampleFormControlInput1"><strong>Descrição:</strong></label>
+            <label for="descricao"><strong>Descrição</strong></label>
             <b-form-textarea
-            id="textarea"
-            v-model="form.descricao"
-            placeholder="Digite a descrição"
-            rows="3"
-            max-rows="6"
+              required id="descricao"
+              v-model="form.descricao" placeholder="Digite a descrição"
+              rows="3" max-rows="10"
             ></b-form-textarea>
           </div>
           <div class="form-group">
-            <label for="exampleFormControlInput1"><strong>Local:</strong></label>
-            <input type="text" class="form-control" placeholder="Digite o local" v-model="form.local" />
+            <label for="local"><strong>Local</strong></label>
+            <input required 
+              id="local" type="text" 
+              class="form-control" placeholder="Digite o local" 
+              v-model="form.local" />
           </div>
           <div>
             <b-row>
               <b-col>
                 <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Carga horária:</strong> <em>(em horas)</em></label>
-                  <input type="number" class="form-control"  v-model="form.qtdCargaHoraria" />
+                  <label for="inicioInscricoes"><strong>Início de inscrições</strong></label>
+                  <b-form-datepicker
+                    id="inicioInscricoes" v-model="form.d_inscricao"
+                    class="mb-2" :min="minDate"
+                    locale="pt-br" placeholder="Escolha uma data"
+                    required=true
+                  ></b-form-datepicker>
                 </div>
               </b-col>
               <b-col>
                 <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Quantidade de dias:</strong></label>
-                  <input type="number" min="0" pattern="\d+" class="form-control" v-model="form.qtdDias" />
-                </div>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col>
-                <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Quantidade de vagas:</strong></label>
-                  <input type="number" min="0" pattern="\d+" class="form-control" v-model="form.qtdVagas" />
-                </div>
-              </b-col>
-              <b-col>
-                <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Valor da inscrição:</strong></label>
-                  <input type="number" min="0" pattern="\d+" class="form-control" v-model="form.valor" />
-                </div>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col>
-                <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Data de início de inscrições:</strong></label>
-                  <input type="date" class="form-control" v-model="form.d_inscricao" />
-                </div>
-              </b-col>
-              <b-col>
-                <div class="form-group">
-                  <label for="exampleFormControlInput1"><strong>Data de fim de inscrições:</strong></label>
-                  <input type="date" :min="form.d_inscricao" class="form-control" v-model="form.d_inscricao_fim" />
+                  <label for="fimInscricoes"><strong>Fim de inscrições</strong></label>
+                  <b-form-datepicker
+                    id="fimInscricoes"
+                    v-model="form.d_inscricao_fim" :min="form.d_inscricao"
+                    class="mb-2" locale="pt-br" placeholder="Escolha uma data"
+                    required :disabled="disabledDataInscricao"
+                  ></b-form-datepicker>
                 </div>
               </b-col>
             </b-row>            
+            <b-row>
+              <b-col>
+                <div class="form-group">
+                  <label for="inicioEvento"><strong>Início do evento</strong></label>
+                  <b-form-datepicker
+                    id="inicioEvento" v-model="form.d_evento_inicio"
+                    :min="minDate" class="mb-2"
+                    locale="pt-br" placeholder="Escolha uma data"
+                  ></b-form-datepicker>
+                </div>
+              </b-col>
+              <b-col>
+                <div class="form-group">
+                  <label for="fimEvento"><strong>Fim do evento</strong></label>
+                  <b-form-datepicker
+                    id="fimEvento" v-model="form.d_evento_fim"
+                    :min="form.d_evento_inicio" class="mb-2"
+                    locale="pt-br" placeholder="Escolha uma data"
+                    :disabled="disabledDataEvento"
+                  ></b-form-datepicker>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div class="form-group">
+                  <label for="cargaHoraria"><strong>Carga horária</strong> <em>(em horas)</em></label>
+                  <input id="cargaHoraria" placeholder="0" type="number" 
+                  class="form-control"  v-model="form.qtdCargaHoraria" required/>
+                </div>
+              </b-col>
+              <b-col>
+                <div class="form-group">
+                  <label for="qtdDias"><strong>Quantidade de dias</strong></label>
+                  <input 
+                    id="qtdDias" type="number" 
+                    placeholder="0" min="0" 
+                    pattern="\d+" class="form-control" 
+                    v-model="form.qtdDias" required/>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div class="form-group">
+                  <label for="qtdVagas"><strong>Quantidade de vagas</strong></label>
+                  <input 
+                    id="qtdVagas" placeholder="0" 
+                    type="number" min="0" 
+                    pattern="\d+" class="form-control" 
+                    v-model="form.qtdVagas" required/>
+                </div>
+              </b-col>
+              <b-col>
+                <div class="form-group">
+                  <label for="valorInscricoes"><strong>Valor da inscrição</strong> <em>(em reais)</em></label>
+                  <input 
+                    id="valorInscricoes" placeholder="0" 
+                    type="number" min="0" pattern="\d+" 
+                    class="form-control" v-model="form.valor" required/>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div class="form-group">
+                  <label for="inicioRolagem"><strong>Início da rolagem</strong></label>
+                  <b-form-datepicker
+                    id="inicioRolagem" v-model="form.inicio_rolagem"
+                    class="mb-2" :min="minDate"
+                    locale="pt-br" placeholder="Escolha uma data" required
+                  ></b-form-datepicker>
+                </div>
+              </b-col>
+              <b-col>
+                <div class="form-group">
+                  <label for="fimRolagem"><strong>Fim da rolagem</strong></label>
+                  <b-form-datepicker
+                    :disabled="disabledDataRolagem"
+                    id="fimRolagem" v-model="form.fim_rolagem"
+                    :min="form.inicio_rolagem" class="mb-2" locale="pt-br"
+                    placeholder="Escolha uma data"  required
+                  ></b-form-datepicker>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div class="form-group">
+                  <label for="diasCompensacao"><strong>Dias de compensação</strong></label>
+                  <input 
+                    id="diasCompensacao" type="number" 
+                    min="0" pattern="\d+" 
+                    placeholder="0" class="form-control" 
+                    v-model="form.dias_compensacao" />
+                </div>
+              </b-col>
+              <b-col>
+                <label for="anexoParticipantes"><strong>Anexos pelos participantes</strong></label>
+                <div class="form-group">
+                  <b-form-checkbox
+                    id="anexoParticipantes"
+                    v-model="form.participante_anexos"
+                  > Sim, será necessário. 
+                  </b-form-checkbox>
+                </div>
+              </b-col>
+            </b-row>
+          </div>          
+          <div class="form-group">
+            <label for="textoDeclaracao"><strong>Texto de declaração</strong></label>
+            <b-form-textarea
+              id="textoDeclaracao" v-model="form.textoDeclaracaoEvento"
+              placeholder="Digite o texto de declaração do evento"
+              rows="3"  max-rows="6"
+            ></b-form-textarea>
           </div>
-          
+          <div class="form-group">
+            <b-form-checkbox
+              v-model="form.ativo"
+            > Eu quero ativar o evento durante a criação. 
+            </b-form-checkbox>
+          </div>
           <div class="form-group">
             <b-button type="submit" variant="primary"><i class="fa fa-dot-circle-o"></i> Enviar</b-button>
             <b-button type="reset" variant="danger"><i class="fa fa-ban"></i> Limpar campos</b-button>
@@ -91,28 +198,48 @@
 <script>
 import axios from "~/axios";
 import Swal from "sweetalert2";
+import moment from "moment";
 
 export default {
   layout: "menu/tutor",
   data() {
     return {
       form: {
-        titulo: "",
-        descricao: "",
-        local: "",
-        qtdCargaHoraria: 0,
-        qtdDias: 0,
-        qtdVagas: 0,
-        valor: 0,
-        d_inscricao_fim: "",
+        d_evento_fim: "",
+        d_evento_inicio: "",
         d_inscricao: "",
+        d_inscricao_fim: "",
+        descricao: "",
+        dias_compensacao: "",
+        fim_rolagem: "",
+        inicio_rolagem: "",
+        local: "",
+        participante_anexos: false,
+        percentual: 0,
+        qtdCargaHoraria: "",
+        qtdDias: "",
+        qtdVagas: "",
+        textoDeclaracaoEvento: "",
+        titulo: "",
+        valor: "",
         ativo: false
       },
-      alert: {
-        message: "",
-        class: ""
-      }
+      minDate: null
     };
+  },
+  mounted() {
+    this.minDate = moment().format("YYYY-MM-DD");
+  },
+  computed: {
+    disabledDataRolagem() {
+      return this.form.inicio_rolagem === ''; 
+    },
+    disabledDataInscricao(){
+      return this.form.d_inscricao === ''; 
+    },
+    disabledDataEvento(){
+      return this.form.d_evento_inicio === ''; 
+    }
   },
   methods: {
     goToEventosCadastrados() {
@@ -142,19 +269,40 @@ export default {
         });
     },
     onReset(evt) {
-      evt.preventDefault()
-      // Reset our form values
-      this.form.titulo = ""
-      this.form.descricao = ""
-      this.form.local = ""
-      this.form.qtdCargaHoraria = 0
-      this.form.qtdDias = 0
-      this.form.qtdVagas = 0
-      this.form.valor = 0
-      this.form.d_inscricao = "mm/dd/yyyy"
-      this.form.d_inscricao_fim = "mm/dd/yyyy"
-      this.form.ativo= false
+      evt.preventDefault()      
+      this.d_evento_fim = ""
+      this.d_evento_inicio = ""
+      this.d_inscricao = ""
+      this.d_inscricao_fim = ""
+      this.descricao = ""
+      this.dias_compensacao = ""
+      this.fim_rolagem = ""
+      this.inicio_rolagem = ""
+      this.local = ""
+      this.participante_anexos = false
+      this.qtdCargaHoraria = ""
+      this.qtdDias = ""
+      this.qtdVagas = ""
+      this.textoDeclaracaoEvento = ""
+      this.titulo = ""
+      this.valor = ""
+      this.ativo = false
     }
   }
 };
 </script>
+
+<style scoped>
+h2 {
+  font-weight: 300;
+}
+
+strong {
+  color: gray;
+}
+
+em {
+  color: gray;
+  font-weight: 500;
+}
+</style>
--- pages/tutor/eventos/eventos-cadastrados/index.vue
+++ pages/tutor/eventos/eventos-cadastrados/index.vue
@@ -4,7 +4,7 @@
       <template v-slot:header>
         <b-row>
           <b-col>
-            <h3><i class="fa fa-calendar-check-o px-2"></i>Eventos cadastrados</h3>
+            <h2><i class="fa fa-calendar-check-o px-2"></i>Eventos cadastrados</h2>
           </b-col>
           <b-col>
             <nuxt-link
@@ -15,80 +15,84 @@
           </b-col>
         </b-row>
       </template>
-      <div v-if="eventos.length > 0">
-          
-       <b-input-group  class="mt-3 mb-3" >
-          <b-form-input
-            v-model="keyword"
-            placeholder="Busca por título"            
-            type="text"
-          ></b-form-input>
-          <b-input-group-text slot="append">
-            <b-btn class="p-0" :disabled="!keyword" variant="link" size="sm" @click="keyword = ''"><i class="fa fa-remove"></i></b-btn>
-        </b-input-group-text>
-        </b-input-group>
-
-        <b-table
-          responsive="sm"
-          :items="items"
-          :current-page="currentPage"
-          :bordered="true"
-          :per-page="10"
-          :fields="fields"
-        >
-          <template v-slot:cell(ativo)="row">
-            <b-form-checkbox
-                size="lg"
-                v-model="row.item.ativo"
-                disabled
-              ></b-form-checkbox>
-          </template>
-          <template v-slot:cell(pages)="row">
-            <nuxt-link
-              :to="`/tutor/eventos/organizadores/?idEvento=${row.item.idEvento}`"
-              class="btn btn-sm btn-teal"
-              style="color: white" 
-            ><i class="fa fa-group fa-fw"></i> Organizadores</nuxt-link>
-            <b-button
-              :to="`/tutor/eventos/participantes/?idEvento=${row.item.idEvento}`"
-              class="btn btn-sm mt-2"
-              variant="secondary"
-            ><i class="fa fa-group fa-fw"></i> Participantes</b-button>
-          </template>
-          <template v-slot:cell(actions)="row">
-            <b-button
-              class="btn btn-sm btn-success mt-2"
-              v-if="row.item.ativo === false"
-              @click.prevent="ativar(row.item.idEvento)"
-            ><i class="fa fa-check fa-fw"></i> Ativar</b-button>
-            <nuxt-link
-              class="btn btn-sm btn-cyan mt-2"
-              :to="`/tutor/eventos/eventos-cadastrados/${row.item.idEvento}`"
-            ><i class="fa fa-eye fa-fw"></i> Informações</nuxt-link>
-            <nuxt-link
-              class="btn btn-sm btn-warning mt-2"
-              :to="`/tutor/eventos/eventos-cadastrados/edit/${row.item.idEvento}`"              
-            ><i class="fa fa-pencil fa-fw"></i> Editar</nuxt-link>
-            <b-button
-              @click.prevent="del(row.item.idEvento, row.index)"
-              class="btn btn-sm btn-danger mt-2"
-            ><i class="fa fa-trash-o fa-fw"></i> Remover</b-button>
-          </template>
-        </b-table>
-        <nav>
-          <b-pagination
-            :total-rows="items.length"
-            :per-page="10"
-            pills
-            v-model="currentPage"
-            prev-text="Anterior"
-            next-text="Próximo"
-            hide-goto-end-buttons
-          />
-        </nav>
+      
+      <div v-if="eventosLoading === true" class="d-flex justify-content-center mb-3">
+        <h4>Carregando...</h4>
+        <b-spinner style="width: 3rem; height: 3rem;" type="grow" variant="primary" label="Large Spinner"></b-spinner>
       </div>
       <div v-else>
-        <h5>Nenhum evento cadastrado</h5>
+        <div v-if="eventos.length > 0">
+            
+          <b-input-group  class="mt-3 mb-3" >
+            <b-form-input
+              v-model="keyword"
+              placeholder="Busca por título"            
+              type="text"
+            ></b-form-input>
+            <b-input-group-text slot="append">
+              <b-btn class="p-0" :disabled="!keyword" variant="link" size="sm" @click="keyword = ''"><i class="fa fa-remove"></i></b-btn>
+          </b-input-group-text>
+          </b-input-group>
+
+          <b-table
+            responsive="sm"
+            :items="items"
+            :current-page="currentPage"
+            :bordered="true"
+            :per-page="10"
+            :fields="fields"
+          >
+            <template v-slot:cell(pages)="row">
+              <nuxt-link
+                :to="`/tutor/eventos/organizadores/?idEvento=${row.item.idEvento}`"
+                class="btn btn-sm btn-teal"
+                style="color: white" 
+              ><i class="fa fa-group fa-fw"></i> Organizadores</nuxt-link>
+              <b-button
+                :to="`/tutor/eventos/participantes/?idEvento=${row.item.idEvento}`"
+                class="btn btn-sm mt-2"
+                variant="secondary"
+              ><i class="fa fa-group fa-fw"></i> Participantes</b-button>
+            </template>
+            <template  v-slot:cell(actions)="row">
+              <b-button
+                class="btn btn-sm btn-success mt-2"
+                v-if="row.item.ativo === false"
+                @click.prevent="ativar(row.item.idEvento)"
+              >
+                <i class="fa fa-check fa-fw"></i> Ativar
+              </b-button>
+              <nuxt-link
+                class="btn btn-sm btn-cyan mt-2"
+                :to="`/tutor/eventos/eventos-cadastrados/${row.item.idEvento}`"
+              ><i class="fa fa-eye fa-fw"></i> Informações</nuxt-link>
+              <nuxt-link
+                class="btn btn-sm btn-warning mt-2"
+                :to="`/tutor/eventos/eventos-cadastrados/edit/${row.item.idEvento}`"              
+              ><i class="fa fa-pencil fa-fw"></i> Editar</nuxt-link>
+              <b-button
+                @click.prevent="del(row.item.idEvento, row.index)"
+                class="btn btn-sm btn-danger mt-2"
+              >
+                <i class="fa fa-trash-o fa-fw"></i> Remover
+              </b-button>
+            </template>
+          </b-table>
+          <nav>
+            <b-pagination
+              :total-rows="items.length"
+              :per-page="10"
+              pills
+              v-model="currentPage"
+              prev-text="Anterior"
+              next-text="Próximo"
+              hide-goto-end-buttons
+            />
+          </nav>
+        </div>
+        <div v-else>
+          <h5>Nenhum evento cadastrado</h5>
+        </div>
       </div>
     </b-card>
   </div>
@@ -98,20 +102,25 @@
 
 import axios from "~/axios";
 import Swal from "sweetalert2";
+import moment from "moment";
 
 export default {
   name: "dashboard",
   layout: "menu/tutor",
   data() {
     return {
+      eventosLoading: true,
+      eventosAtivarLoading: false,
+      eventosRemoverLoading: false,
       keyword: '',
       eventos: [],
       currentPage: 1,
       fields: [
         { key: "titulo", sortable: true, label: "Título"  },
-        { key: "d_inscricao", sortable: true, label: "Início das inscrições" , formatter: (value) => { if (value != null) return new Intl.DateTimeFormat('pt-BR').format(new Date(value))} },
-        { key: "d_inscricao_fim", sortable: true, label: "Fim das inscrições" , formatter: (value) => { if (value != null) return new Intl.DateTimeFormat('pt-BR').format(new Date(value))} },
-        { key: "ativo", sortable: true, label: "Ativo"  },
+        { key: "d_inscricao", sortable: true, label: "Início das inscrições" , formatter: (date) => { if (date != null) return moment(date).format('DD/MM/YYYY') } },
+        { key: "d_inscricao_fim", sortable: true, label: "Fim das inscrições" , formatter: (date) => { if (date != null) return  moment(date).format('DD/MM/YYYY') } },
+        { key: "d_evento_inicio", sortable: true, label: "Início do evento" , formatter: (date) => { if (date != null) return moment(date).format('DD/MM/YYYY') } },
+        { key: "d_evento_fim", sortable: true, label: "Fim do eventos" , formatter: (date) => { if (date != null) return  moment(date).format('DD/MM/YYYY') } },
         { key: "pages", sortable: true, label: "Páginas disponíveis"  },
         { key: "actions", sortable: true, label: "Ações disponíveis"  }
       ]
@@ -133,6 +142,7 @@ export default {
         .get("eventos")
         .then(res => {
           this.eventos = res.data.content;
+          this.eventosLoading = false;
         })
         .catch( err => {
           if (err.response.status === 404) {
@@ -169,19 +179,25 @@ export default {
             title: 'Evento removido',
             icon: 'success',
           })
-          .then( () => {
-            this.eventos.splice(rowId, 1);
-          });
+          .then( () => this.eventos.splice(rowId, 1) );
         })
         .catch(err => {
-          Swal.fire({
-            title: 'Evento não removido',
-            icon: 'error'
-          })
+          if (err.response.status === 500) {
+            Swal.fire({
+              title: 'Evento não removido',
+              text: 'Não é possível remover eventos com participantes ou organizadores, remova-os antes.',
+              icon: 'error'
+            })
+          } 
+          else {
+            Swal.fire({
+              title: 'Evento não removido',
+              icon: 'error'
+            })
+          }
         });
     },
     ativar(id) {
-      e.preventDefault();
       axios
         .post(`eventos-ativar/${id}`)
         .then( () => {
@@ -203,3 +219,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+h2, h4 {
+  font-weight: 300;
+}
+</style>
