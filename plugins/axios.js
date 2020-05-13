b63dd8f337784074150134899be4ecc27a1f087f
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-05-13 00:17:55 -0300

    Remove comentários e consoles

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,9 +2,7 @@ import Cookies from "js-cookie";
 
 export default function ({ $axios, redirect }) {
     $axios.onRequest(config => {
-        console.log('hue');
         config.headers.common['Authorization'] = Cookies.get("auth");
         $axios.setHeader('Authorization', Cookies.get("auth"));
     });
-    //$axios.setHeader('Authorization', `${Cookies.get("auth")}`);
 }
