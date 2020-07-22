4d86d83b7562abe2af69a86aeb42d5051a530a09
Samuellucas97 <samuellucas97@ufrn.edu.br>
2020-07-22 00:37:49 -0300

    Corrigindo erros de data do perfil de usuário e adicionando upload de foto de perfil

--- pages/tutor/meus-dados/index.vue
+++ pages/tutor/meus-dados/index.vue
@@ -19,10 +19,23 @@
       ></b-spinner>
     </div>
     <div v-else>
-      <b-form @submit.prevent="onSubmit"> 
-        <b-avatar class="mb-3"  size="8em">
-           <template v-slot:badge><i class="fa fa-edit"></i></template>
-        </b-avatar>
+      <b-form @submit.prevent="onSubmit">
+        <div v-if="form.data_ingresso != null">
+          Período de participação: <b>{{form.data_ingresso | moment}}</b> - <b v-if="form.data_egresso != null">{{form.data_egresso | moment}}</b> 
+        </div>
+        <div class="row justify-content-center d-flex  align-items-center mb-3">
+            <b-avatar :src="`${form.foto}`" class="mb-3" size="10em"></b-avatar>
+        </div> 
+
+        <b-form-file
+              v-model="file"
+              placeholder="Nenhuma foto selecionada" browse-text="Selecionar foto" id="anexo"></b-form-file>
+            <b-form-text class="mb-1"> O tamanho máximo da foto é de 10 megabytes. </b-form-text>
+            <b-progress :value="progressValue" :max="100"  show-progress animated></b-progress>
+            <b-button block @click="fazerUploadImagem" 
+              class="btn btn-sm btn-success mt-2 mb-4">
+              Carregar foto
+            </b-button> 
 
         <b-form-group for="nome" label="Nome completo">
           <b-form-input id="nome" v-model="form.pessoa.nome" required></b-form-input>
@@ -58,16 +71,20 @@
 <script>
 import Cookies from "js-cookie";
 import Swal from "sweetalert2";
+import moment from "moment";
 
 export default {
   name: "dashboard",
   layout: "menu/tutor",
   data() {
     return {
+      file:[],
+      progressValue: 0,
       isLoading: true,
       form: {
         pessoa: {
           nome: "",
+          foto: "",
           usuario: {
             email: ""
           }
@@ -75,11 +92,48 @@ export default {
       }
     };
   },
-
+  filters: {
+    moment: function(date) {
+      return moment(date).format("DD/MM/YYYY");
+    }
+  },
   mounted() {
     this.getInfo();
   },
   methods: {
+    fazerUploadImagem(evento) {
+      const formData = new FormData()
+      formData.append("file", this.file)
+      this.$axios
+        .post("https://epet.imd.ufrn.br:8443/uploadfile",formData, {
+          onUploadProgress: uploadEvent => {
+            this.progressValue = `${Math.round(uploadEvent.loaded/ uploadEvent.total * 100)}%`
+          }
+        })
+        .then(res => {
+          this.form.foto = res.data;
+
+          Swal.fire({
+            title: "PDF de template atualizado",
+            icon: "success"
+          })
+          .then( () => {
+            this.progressValue = 0
+            this.file = []
+          });
+        })
+        .catch(err => {
+          Swal.fire({
+            title: "PDF de template não atualizado",
+            text: "Tente novamente em outro momento.",
+            icon: "error"
+          })
+          .then( () => {
+            this.progressValue = 0
+            this.file = []
+          });
+        });
+    },
     getInfo() {
        this.$axios
         .get(`/petianos-pessoa/${this.$store.state.profile.idPessoa}`)
@@ -101,7 +155,7 @@ export default {
       this.$axios
         .post("pessoas-atualizar/", {
           ...this.$store.state.profile,
-          nome: this.form.pessoa.nome
+          nome: this.form.pessoa.nome,
         })
         .catch(err => {
           this.submitAlert(true);
