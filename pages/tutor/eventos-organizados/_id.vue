7343ac75b2bc8049a90e42acfdc9867a9ba8ea18
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-07-29 22:00:21 -0300

    Inicia implementação de eventos organizados

--- pages/tutor/eventos-organizados/_id.vue
+++ pages/tutor/eventos-organizados/_id.vue
@@ -17,13 +17,13 @@
           <h5>Título:</h5> <h6> {{form.evento.titulo}}</h6>
           <p class="mt-3 mb-1">
             <strong>Perído de inscrições:</strong>
-            <span v-if="form.evento.d_inscricao !== ''">{{ this.form.d_inscricao | moment }}</span> -
-            <span v-if="form.evento.d_inscricao_fim !== ''">{{ this.form.d_inscricao_fim | moment}}</span>
+            <span v-if="form.evento.d_inscricao !== ''">{{ form.evento.d_inscricao | moment }}</span> -
+            <span v-if="form.evento.d_inscricao_fim !== ''">{{ form.evento.d_inscricao_fim | moment}}</span>
           </p>
           <p class="mt-0 mb-1">
             <strong>Perído de realização do evento:</strong>
-            <span v-if="form.d_evento_inicio !== ''">{{ this.form.d_evento_inicio | moment }}</span> -
-            <span v-if="form.d_evento_inicio !== ''">{{ this.form.d_evento_fim | moment}}</span>
+            <span v-if="form.d_evento_inicio !== ''">{{ form.evento.d_evento_inicio | moment }}</span> -
+            <span v-if="form.d_evento_inicio !== ''">{{ form.evento.d_evento_fim | moment}}</span>
           </p>
           <p class="mt-0 mb-1">
             <strong>Quantidade de dias de evento:</strong>
@@ -40,8 +40,8 @@
           <p class="mt-0 mb-1">
             <strong>Valor da inscrição:</strong>
             {{new Intl
-                .NumberFormat([], { style: 'currency', currency: 'BRL'})           
-                .format(form.evento.valor) }}
+            .NumberFormat([], { style: 'currency', currency: 'BRL'})
+            .format(form.evento.valor) }}
           </p>
           <p class="mt-0 mb-1">
             <strong>Local do curso:</strong>
@@ -51,17 +51,14 @@
             <strong>Descrição:</strong>
             {{form.evento.descricao}}
           </p>
-          
-        </div>  
+
+        </div>
       </b-card-body>
       <template v-slot:footer>
         <b-button id="tooltip-target-1" :disabled="disabledBotaoCertificado"
-         @click.prevent="gerarCertificado()" block variant="success">           
-         <i class="fa fa-certificate mr-1"></i>Emitir certificado de organização
+                  @click.prevent="gerarCertificado()" block variant="success">
+          <i class="fa fa-certificate mr-1"></i>Emitir certificado de organização
         </b-button>
-        <b-tooltip target="tooltip-target-1" triggers="hover">
-          <strong>{{form.evento.percentual}}%</strong> concluído
-        </b-tooltip>
       </template>
     </b-card>
     <b-card header-tag="header">
@@ -77,11 +74,11 @@
           <h4>Carregando...</h4>
           <b-spinner style="width: 3rem; height: 3rem;" type="grow" variant="primary" label="Large Spinner"></b-spinner>
         </div>
-        <div v-else>    
+        <div v-else>
           <p class="mt-0 mb-1">
             <strong>Perído de rolagem:</strong>
-            <span v-if="this.form.inicio_rolagem !== ''">{{ this.form.inicio_rolagem | moment }}</span> -
-            <span v-if="this.form.fim_rolagem !== ''">{{ this.form.fim_rolagem | moment}}</span>
+            <span v-if="this.form.inicio_rolagem !== ''">{{ this.form.evento.inicio_rolagem | moment }}</span> -
+            <span v-if="this.form.fim_rolagem !== ''">{{ this.form.evento.fim_rolagem | moment}}</span>
           </p>
           <p class="mt-0 mb-1">
             <strong>Quantidade de dias de compensação:</strong>
@@ -90,7 +87,7 @@
           <p class="mt-0 mb-1">
             <strong>Há anexo para os participantes:</strong>
             <span v-if="form.participante_anexos === true ">Sim.</span>
-            <span v-else>Não.</span> 
+            <span v-else>Não.</span>
           </p>
           <p class="mt-3 mb-2">
             <strong>Texto de declaração do participante:</strong>
@@ -108,105 +105,121 @@
 
 <script>
 
-import Swal from "sweetalert2";
-import moment from "moment";
+  import Swal from "sweetalert2";
+  import moment from "moment";
 
-export default {
-  layout: "menu/tutor",
-  data() {
-    return {
-      isLoading: true,
-      eventoTerminou: true,
-      form: {  
-        evento: { 
-          titulo: "", 
-          descricao: "", 
-          local: "", 
-          imagem: null, 
-          d_inscricao: "", 
-          d_inscricao_fim: "", 
-          d_evento_inicio: "", 
-          d_evento_fim: "", 
-          inicio_rolagem: "", 
-          fim_rolagem: "", 
-          dias_compensacao: 0, 
-          percentual: 0, 
-          ativo: true, 
-          participante_anexos: false, 
-          qtdVagas: 5, 
-          qtdCargaHoraria: 2, 
-          qtdDias: 5, 
-          valor: 0, 
-          textoDeclaracaoEvento: "", 
-          textoDeclaracaoEventoOrganizador: "" 
-        }, 
-        data_maxima: "", 
-        confirmado: true, 
-        espera: false, 
-        idParticipantes: 26 
-      } 
-    };
-  },
-  mounted() {
-    this.$axios
-      .get(`organizadores/${this.$route.params.id}`)
-      .then(res => {
-        this.form = res.data;
-        this.$nuxt.$emit("changeCrumbs", this.form.evento.titulo);
-        this.isLoading = false;
-      })
-      .catch(err => {
-        Swal.fire({
-          title: "Houve um problema...",
-          text: "Por favor, tente recarregar a página. Caso não dê certo," + 
-          " tente novamente mais tarde.",
-          icon: "error"
-        })
-        .then(() => this.isLoading = false);
-      });
-  },
-  filters: {
-    moment: function (date) {
-      return moment(date).format('DD/MM/YYYY');
-    }
-  },
-  computed: {
-    disabledBotaoCertificado() {
-      return !(moment(new Date().getDay()) > moment(this.d_inscricao_fim)); 
+  export default {
+    layout: "menu/tutor",
+    data() {
+      return {
+        isLoading: true,
+        eventoTerminou: true,
+        form: {
+          evento: {
+            titulo: "",
+            descricao: "",
+            local: "",
+            imagem: null,
+            d_inscricao: "",
+            d_inscricao_fim: "",
+            d_evento_inicio: "",
+            d_evento_fim: "",
+            inicio_rolagem: "",
+            fim_rolagem: "",
+            dias_compensacao: 0,
+            percentual: 0,
+            ativo: true,
+            participante_anexos: false,
+            qtdVagas: 5,
+            qtdCargaHoraria: 2,
+            qtdDias: 5,
+            valor: 0,
+            textoDeclaracaoEvento: "",
+            textoDeclaracaoEventoOrganizador: ""
+          },
+          data_maxima: "",
+          confirmado: true,
+          espera: false,
+          idParticipantes: 26
+        }
+      };
     },
-  },
-  methods: {
-    gerarCertificado() {
-      if (this.form.percentual >= 75) {
-
-      } else {
-        Swal.fire({
-          title: "Certificado não gerado",
-          icon: "error",
-          text: "É necessário que se obtenha no mínimo 75% de participação para geração do certificado"
+    mounted() {
+      this.$axios
+        .get(`organizadores/${this.$route.params.id}`)
+        .then(res => {
+          this.form = res.data;
+          this.$nuxt.$emit("changeCrumbs", this.form.evento.titulo);
+          this.isLoading = false;
+        })
+        .catch(err => {
+          Swal.fire({
+            title: "Houve um problema...",
+            text: "Por favor, tente recarregar a página. Caso não dê certo," +
+              " tente novamente mais tarde.",
+            icon: "error"
+          })
+            .then(() => this.isLoading = false);
+        });
+    },
+    filters: {
+      moment: function (date) {
+        return moment(date).format('DD/MM/YYYY');
+      }
+    },
+    computed: {
+      disabledBotaoCertificado() {
+        return (moment().isAfter(moment(this.d_inscricao_fim)));
+      },
+    },
+    methods: {
+      gerarCertificado() {
+        console.log('hue');
+        let idPessoa = this.form.pessoa.idPessoa;
+        let idEvento = this.form.evento.idEvento;
+        this.$axios.get(
+          `certificado/gerarOrganizador/${idPessoa}/${idEvento}`,
+          {responseType: 'arraybuffer'}
+        ).then(res => {
+          let fileURL = window.URL.createObjectURL(new Blob([res.data], {type: 'application/*'}));
+          let fileLink = document.createElement('a');
+          let nomeAnexoCorrigido = 'certificado';
+          fileLink.href = fileURL;
+          fileLink.setAttribute('download', nomeAnexoCorrigido);
+          document.body.appendChild(fileLink);
+          fileLink.click();
+        }).catch(err => {
+          Swal.fire({
+            title: "Certificado não gerado",
+            icon: "error",
+            text: "Aconteceu algum problema com a geração do certificado. Tente novamente mais tarde."
+          });
         });
       }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  padding-left: 10px;
-}
-p {
-  font-size: 15px;
-}
-strong {
-  font-size: 16px;
-}
-h3, h4 {
-  font-weight: 300;
-}
-h5, h6 {
-  display: inline;
-  font-size: 18px;
-}
+  ul {
+    list-style: none;
+    padding-left: 10px;
+  }
+
+  p {
+    font-size: 15px;
+  }
+
+  strong {
+    font-size: 16px;
+  }
+
+  h3, h4 {
+    font-weight: 300;
+  }
+
+  h5, h6 {
+    display: inline;
+    font-size: 18px;
+  }
 </style>
